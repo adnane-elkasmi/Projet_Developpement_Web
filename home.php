18585038203d5df71d4e75092e8ae6a19597ada4
Adnane EL KASMI <95549832+adnane-elkasmi@users.noreply.github.com>
2021-12-05 01:08:25 +0100

    Add files via upload

---
+++ home.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="description" content="EISTI Prospection">
+    <meta name="author" content="Projet GM ATILLA">
+    <link rel="shortcut icon" href="Pictures/Site/EISTI.svg">
+    <title> Prospection EISTI </title>
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/main.css" rel="stylesheet">
+  </head>
+
+  <body>
+    <div id="wrap">
+    <div class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="?view=login"><img src="img/home.png" height="55"></a>
+        </div>
+        <div class="navbar-collapse collapse">
+          <ul class="nav navbar-nav navbar-right">
+            <li class=""><a href="https://eisti.fr" target="_blank">Site EISTI</a></li>
+            <li class=""><a href="?view=login">SE CONNECTER</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+
+
+
+  <div id="headerwrap">
+  	<div class="container">
+  		<div class="row centered">
+  			<div class="col-lg-8 col-lg-offset-2">
+  				<h1>Projet ING1 GM<br><b>Atilla : Prospection</b></h1>
+          <br>
+          <h4 style="color: white"><b>QU’EST-CE QUE LA PROSPECTION ?</b></h4>
+          <br>
+          <p style="color: white">La prospection est faite par les élèves-ingénieurs de l’EISTI lors de
+  forums, de salons, de conférences, etc.</p>
+  <br>
+          <h4 style="color: white"><b>OBJECTIFS</b></h4>
+          <br>
+          <p style="color: white">Aider l’administration à gérer la prospection.</p>
+          <p style="color: white">La facilité et la lisibilité grâce à une meilleure gestion des données et un affichage simplifié.</p>
+          <p style="color: white">Eviter la perte de données liée au partage d’un fichier sur le réseau.</p>
+  			</div>
+  		</div>
+  	</div>
+  </div>
+
+  <div class="container w">
+    <div class="row centered">
+      <div class="col-lg-3">
+          <img class="img-circle" src="img/adnane.png" width="110" height="110" />
+        <h4>Adnane EL KASMI</h4>
+        <p>GM2 (CPGE)</p>
+      </div>
+      <div class="col-lg-3">
+          <img class="img-circle" src="img/cpp.png" width="110" height="110" />
+        <h4>Charles-Philippe FRANTZ</h4>
+        <p>GM2 (CPI)</p>
+      </div>
+      <div class="col-lg-3">
+          <img class="img-circle" src="img/aboubacar.png" width="110" height="110" />
+        <h4>Aboubacar KOUROUMA</h4>
+        <p>GM4 (CPGE)</p>
+      </div>
+      <div class="col-lg-3">
+        <img class="img-circle" src="img/christelle.png" width="110" height="110" />
+        <h4>Christelle VO</h4>
+        <p>GM4 (CPI)</p>
+      </div>
+    </div>
+  </div>
+
+  </body>
+</html>
+
+<!-- FOOTER -->
+<div id="f">
+	<div class="container">
+		<div>
+			<div>
+				<h4 style="text-align:center;" >Vous pouvez accéder au cahier des charges en cliquant <a href="cahier_des_charges/cahier_des_charges.pdf" target="_blank">ici</a>.</h4>
+			</div>
+		</div><!-- row -->
+	</div><!-- container -->
+</div><!-- Footer -->
